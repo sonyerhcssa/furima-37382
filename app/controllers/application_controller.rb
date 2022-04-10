416b98bb75a94797177c79ec5a9deba89a5b9bcc
sonyerhcssa <98602149+sonyerhcssa@users.noreply.github.com>
2022-04-10 23:10:36 +0900

    ユーザー管理機能未完了部分の追加
    •db/migrate_20220408132050_devise_create_users.rb
    テーブルのカラム修正とrails db:migrateの実行。
    •rails g devise:viewsの実行
    ダウンロードしていたregistrationsとsessionsのファイルをFURIMAの対象ファイルと置き換え。
    •app/controllers/application_controller.rbファイルに、deviseに関するコントローラーの処理で、新規登録の処理に対して、新たなパラメーターを許可する為に、before_action :configure_permitted_parameters, if: :devise_controller?を記述。
    ログインしていない場合は、ログインページの画面に遷移する記述
    before_action :authenticate_user!
    •app/views/devise/confirmations/new.html.erbに@userとuser_registration_pathを記述。
    •app/views/shared/_header.html.erbにif user_signed_in?を追記し、ログアウト、ログインで異なった表示の仕様を実装。それぞれのパスも追記。
    •デバックツール導入の為、Gemfileにgem 'pry-rails'を記述。インストール
    •ルーティングにresources :usersを記述
    •registrations/new.html.erbのキーをhogeから適切なキーへ変更
    •sessions/new.html.erbのキーをhogeから適切なキーへ変更
    form_withの追加
    •app/models/user.rbにバリデーションを記述
    •app/views/devise/registrations/new.html.erbのエラー文の表示を有効化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  # before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -9,4 +11,10 @@ class ApplicationController < ActionController::Base
       # username == 'abcd' && password == '1234'
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, 
+      :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_day])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :nickname, :birth_day,
+       presence: true
+
+  validates :password,
+       format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+
+
+  validates  :first_name, :last_name, :first_name_kana, :last_name_kana,
+       presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+
 end
