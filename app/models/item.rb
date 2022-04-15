5368497891d2d82582e3101c9e429318afca706e
sonyerhcssa <98602149+sonyerhcssa@users.noreply.github.com>
2022-04-16 00:05:22 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :item_status, :postage, :prefecture, :item_send
+
   belongs_to       :user
   has_one          :purchase
   has_one_attached :image
 
-  validates :item_name, :price, :explanation, :postage_id, :category_id,
-            :item_status_id, :prefecture_id, :item_send_id, :image,
+  validates :item_name, :price, :explanation, :image,
             presence: true
+
+  validates :category_id, :item_status_id, :postage_id, :prefecture_id, :item_send_id,
+            presence: true, numericality: { other_than: 1, message: "can't be blank" } 
 
 end
