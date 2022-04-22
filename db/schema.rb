028f12d83c9190055179daa227715c00b46e91d8
sonyerhcssa <98602149+sonyerhcssa@users.noreply.github.com>
2022-04-22 20:06:04 +0900

    商品一覧表示を、投稿日時で並び替えできるように修正

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2022_04_14_071809) do
     t.integer "prefecture_id", null: false
     t.integer "item_send_id", null: false
     t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
