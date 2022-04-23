cc725c5d8c496f079d90496607cee9b26d6765e0
sonyerhcssa <98602149+sonyerhcssa@users.noreply.github.com>
2022-04-23 23:06:01 +0900

    URLを直接入力しての遷移に条件を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.order('created_at desc')
@@ -42,5 +43,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :price, :explanation, :postage_id, :category_id, :item_status_id, :prefecture_id,
                                  :item_send_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
