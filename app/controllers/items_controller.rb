c834ea299e00d995af34fc34b79ca2a25c720de8
sonyerhcssa <98602149+sonyerhcssa@users.noreply.github.com>
2022-04-25 00:40:11 +0900

    bundle exec rubocop -a を実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,11 +22,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    
   end
 
   def edit
-   
   end
 
   def update
@@ -55,7 +53,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    binding.pry
     redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
   end
 end
