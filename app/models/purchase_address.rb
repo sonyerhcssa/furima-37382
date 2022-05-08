eb6bd83bd8835eee04bf95e45b5b42d4eb1d248b
sonyerhcssa <98602149+sonyerhcssa@users.noreply.github.com>
2022-05-08 21:20:10 +0900

    attr_accessorの中の:purchase_idの削除

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,7 +1,7 @@
 class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address,
-                :building, :phone_number, :purchase_id, :token
+                :building, :phone_number, :token
 
   with_options presence: true do
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
