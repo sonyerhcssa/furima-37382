808c98f7e5dbbd4b6a48267f11ad944905d1fc49
sonyerhcssa <98602149+sonyerhcssa@users.noreply.github.com>
2022-05-12 18:00:56 +0900

    カテゴリーの項目修正。都道府県の入力が無い場合は、購入ができないようにコード修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -5,7 +5,7 @@ class PurchaseAddress
 
   with_options presence: true do
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
     validates :phone_number, numericality: { only_integer: true }, format: { with: /\A\d{10,11}\z/ }
