3ebaee02fa96cfda57b31064449a56b4bed5b49c
sonyerhcssa <98602149+sonyerhcssa@users.noreply.github.com>
2022-05-14 14:27:34 +0900

    商品出品機能の自動計算される「手数料と利益の合計」が不一致を修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,13 +1,14 @@
-function item(){
+window.addEventListener('load', () => {
   const commission = 0.1;
-
   const priceInput = document.getElementById("item-price");
+
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
+
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue * commission);
 
+    const profit_price = document.getElementById("profit");
+    profit_price.innerHTML = inputValue - addTaxDom.innerHTML;
   })
-}
-
-window.addEventListener('load', item);
+});
