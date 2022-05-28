d35ee51fea982eac13033b7c42e6625ea1b936ee
sonyerhcssa <98602149+sonyerhcssa@users.noreply.github.com>
2022-05-28 19:11:40 +0900

    カード情報登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable- has_many :items
   has_many :items
-  #   has_many :purchases
+  has_one :card, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'cards/new'
   devise_for :users
   root to: "items#index"
+
   resources :items do
     resources :purchases, only:[:index, :create]
   end
+
+  resources :cards, only: [:new, :create]
+  resources :users, only: [:show]
 
 end
