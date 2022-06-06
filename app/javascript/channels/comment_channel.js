7eea00ab9dac8d91e886846a39eec148760c9f0b
sonyerhcssa <98602149+sonyerhcssa@users.noreply.github.com>
2022-06-07 00:44:54 +0900

    comment_channel.jsを編集

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -1,15 +1,27 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("CommentChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+if(location.pathname.match(/\/items\/\d/)){
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+  consumer.subscriptions.create("CommentChannel", {
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+
+    received(data) {
+      console.log(data) //削除
+      const html = `
+        <div class="comment">
+          <p class="user-info">${data.user.nickname}： </p>
+          <p>${data.comment.text}</p>
+        </div>`
+      const comments = document.getElementById("comments")
+      comments.insertAdjacentHTML('beforeend', html)
+      const commentForm = document.getElementById("comment-form")
+      commentForm.reset();
+    }
+  })
+}
