eb056e79a89c33675a5c81e0793dc3877f1d8da3
sonyerhcssa <98602149+sonyerhcssa@users.noreply.github.com>
2022-06-07 16:15:07 +0900

    Revert "commentsコントローラーを編集"
    This reverts commit 37a6f1c39f541262c0ffbc463af1e1d859a187ea.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,8 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.new(comment_params)​​
-    @item = Item.find(params[:item_id])
+    @comment = Comment.new(comment_params)
     if @comment.save
-      CommentChannel.broadcast_to @item, { comment: @comment, user: @comment.user }
+      ActionCable.server.broadcast "comment_channel", {comment: @comment, user: @comment.user}
     end
   end
 
